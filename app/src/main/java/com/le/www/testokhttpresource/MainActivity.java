6533ea9b86da02b7c14ab439eda8e43af1c8ff9d
wangweijun1 <wangweijun1@le.com>
2017-04-17 14:21:44 +0800

    add more activity

--- app/src/main/java/com/le/www/testokhttpresource/MainActivity.java
+++ app/src/main/java/com/le/www/testokhttpresource/MainActivity.java
@@ -2,6 +2,7 @@ package com.le.www.testokhttpresource;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -174,6 +175,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+    }
+
+
+    public void more(View v) {
+        startActivity(new Intent(getApplicationContext(), MoreActivity.class));
     }
 
 
---
+++ app/src/main/java/com/le/www/testokhttpresource/MoreActivity.java
@@ -0,0 +1,18 @@
+package com.le.www.testokhttpresource;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by wangweijun1 on 2017/4/17.
+ */
+
+public class MoreActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
